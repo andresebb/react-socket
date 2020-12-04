3b5a7fe5e5b5da1b68bd46893f8f49f61cd211b8
Andres Betancourt <aebb001@gmail.com>
2020-12-03 21:52:55 -0400

    Modelos para manejar nuestras bandas

---
+++ 02-band-names-server/models/band-list.js
@@ -0,0 +1,45 @@
+const Band = require("./band");
+
+class BandList {
+  constructor() {
+    this.bands = [
+      new Band("Metalica"),
+      new Band("Pop"),
+      new Band("Bon Jovi"),
+      new Band("Mana"),
+    ];
+  }
+
+  addBand(name) {
+    const newBand = new Band();
+  }
+
+  removeBand(id) {
+    this.bands.filter((band) => band.id !== id);
+  }
+
+  getBand() {
+    return this.bands;
+  }
+
+  increseVotes(id) {
+    this.bands.map((band) => {
+      if (band.id === id) {
+        band.votes += 1;
+      }
+
+      return band;
+    });
+  }
+
+  changeName(id, newName) {
+    this.bands.map((band) => {
+      if (band.id === id) {
+        band.name = newName;
+      }
+      return band;
+    });
+  }
+}
+
+module.exports = BandList;
