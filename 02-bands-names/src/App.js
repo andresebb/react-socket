4bf734639cf1f6cdf2e29d3ec5813e29c230f89b
Andres Betancourt <aebb001@gmail.com>
2020-12-03 22:32:31 -0400

    Creando estilos del frontend

---
+++ 02-bands-names/src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import BandAdd from "./components/BandAdd";
+import BandList from "./components/BandList";
+
+const App = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="alert">
+          <p>
+            Service Status:
+            <span className="text-success">Online</span>
+            <span className="text-danger">Offline</span>
+          </p>
+        </div>
+
+        <h1>BandNames</h1>
+        <hr />
+
+        <div className="row">
+          <div className="col-8">
+            <BandList />
+          </div>
+
+          <div className="COL-4">
+            <BandAdd />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ 02-bands-names/src/components/BandAdd.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const BandAdd = () => {
+  return (
+    <div>
+      <h1>Agregar Bandas</h1>
+
+      <form action="">
+        <input
+          className="form-control"
+          type="text"
+          name=""
+          id=""
+          placeholder="Agregar una nueva banda"
+        />
+      </form>
+    </div>
+  );
+};
+
+export default BandAdd;
