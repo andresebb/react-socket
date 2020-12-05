319f5edbfaa37aa225c06e83a46a269d5f1dc56c
Andres Betancourt <aebb001@gmail.com>
2020-12-05 14:50:55 -0400

    Pantalla de escritorio

--- 03-ticket-app/src/pages/Escritorio.js
+++ 03-ticket-app/src/pages/Escritorio.js
@@ -1,5 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { Row, Col, Typography, Button, Divider } from "antd";
+import { CloseCircleOutlined, RightOutlined } from "@ant-design/icons";
+
+const { Title, Text } = Typography;
 
 export const Escritorio = () => {
-  return <div>Escritotio</div>;
+  const salir = () => {
+    //
+  };
+
+  const siguienteTicket = () => {
+    //
+  };
+
+  return (
+    <>
+      <Row>
+        <Col span={20}>
+          <Title level={2}>Agrente Jesus</Title>
+          <Text>Usted está trabajando en el escritorio: </Text>
+          <Text type="success"> usuario escritorio </Text>
+        </Col>
+
+        <Col span={4} align="right">
+          <Button shape="round" type="danger" onClick={salir}>
+            <CloseCircleOutlined />
+            Salir
+          </Button>
+        </Col>
+      </Row>
+
+      <Divider />
+
+      <Row>
+        <Col>
+          <Text>Está atendiendo el ticket número: </Text>
+          <Text style={{ fontSize: 30 }} type="danger">
+            55
+          </Text>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col offset={18} span={6} align="right">
+          <Button onClick={siguienteTicket} shape="round" type="primary">
+            Siguiente
+            <RightOutlined />
+          </Button>
+        </Col>
+      </Row>
+    </>
+  );
 };
--- 03-ticket-app/src/pages/Ingresar.js
+++ 03-ticket-app/src/pages/Ingresar.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
-import { Form, Input, Button, Checkbox, InputNumber } from "antd";
+import {
+  Form,
+  Input,
+  Button,
+  Checkbox,
+  InputNumber,
+  Typography,
+  Divider,
+} from "antd";
 import { SaveOutlined } from "@ant-design/icons";
+
+const { Title, Text } = Typography;
+
 const layout = {
   labelCol: {
     span: 4,
@@ -18,8 +30,11 @@ const tailLayout = {
 };
 
 export const Ingresar = () => {
+  const history = useHistory();
+
   const onFinish = (values) => {
     console.log("Success:", values);
+    history.push("/escritorio");
   };
 
   const onFinishFailed = (errorInfo) => {
@@ -27,47 +42,53 @@ export const Ingresar = () => {
   };
 
   return (
-    <Form
-      {...layout}
-      name="basic"
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-    >
-      <Form.Item
-        label="Nombre del Agente"
-        name="Agente"
-        rules={[
-          {
-            required: true,
-            message: "Por facor ingrese su nombre",
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
+    <>
+      <Title level={2}>Ingresar</Title>
+      <Text>Ingrese su nombre y número de escritorio</Text>
+      <Divider />
 
-      <Form.Item
-        label="Escritorio"
-        name="escritorio"
-        rules={[
-          {
-            required: true,
-            message: "Ingrese el numero de escritorio",
-          },
-        ]}
+      <Form
+        {...layout}
+        name="basic"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
       >
-        <InputNumber min={1} max={99} />
-      </Form.Item>
+        <Form.Item
+          label="Nombre del Agente"
+          name="Agente"
+          rules={[
+            {
+              required: true,
+              message: "Por facor ingrese su nombre",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item {...tailLayout}>
-        <Button type="primary" htmlType="submit" shape="round">
-          <SaveOutlined />
-          Ingresar
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item
+          label="Escritorio"
+          name="escritorio"
+          rules={[
+            {
+              required: true,
+              message: "Ingrese el numero de escritorio",
+            },
+          ]}
+        >
+          <InputNumber min={1} max={99} />
+        </Form.Item>
+
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit" shape="round">
+            <SaveOutlined />
+            Ingresar
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
   );
 };
